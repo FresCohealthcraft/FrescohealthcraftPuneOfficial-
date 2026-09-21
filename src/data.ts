5ad35daf3792801b61b29ae295411f978f415779
Fresco <frescofruit.pune@gmail.com>
2026-09-22 01:43:57 +0530

    Update data.ts

--- src/data.ts
+++ src/data.ts
@@ -121,7 +121,7 @@ import DubaiKunafaPistachioImg from "./assets/images/Dubai-Kunafa-Pistachio.png"
 import chickenPowerBowlImg from "./assets/images/Chicken-Power-Bowl.png";
 
 // @ts-ignore
-import AvocadotoastImg from "./assets/images/avocado_toast.png";
+import AvocadotoastImg from "./assets/images/avocado_toast.PNG";
 // @ts-ignore
 import ProteinChickenSandwichImg from "./assets/images/Chicken-Sandwich.png";
 // @ts-ignore
